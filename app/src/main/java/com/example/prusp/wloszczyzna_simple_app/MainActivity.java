686ba850e90f86ffbebb86b126105d585d88911f
Piotr Prus <prus.piotr@gmail.com>
2017-03-15 23:28:55 +0100

    Added intent that opens the dialer with number already selected.

--- app/src/main/java/com/example/prusp/wloszczyzna_simple_app/MainActivity.java
+++ app/src/main/java/com/example/prusp/wloszczyzna_simple_app/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.prusp.wloszczyzna_simple_app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void contactOnClick(View view) {
+        Intent contactIntent = new Intent(Intent.ACTION_DIAL);
+        contactIntent.setData(Uri.parse("tel:"+getString(R.string.telephone_number)));
+        startActivity(contactIntent);
     }
 }
