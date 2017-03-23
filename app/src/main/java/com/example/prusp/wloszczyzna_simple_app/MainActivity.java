e5b75318614c9ee1f3cdc817a5948a0bf54ba493
Piotr Prus <prus.piotr@gmail.com>
2017-03-23 23:17:41 +0100

    setTheCollapsingToolbarTitle method added to MainActivity.java

--- app/src/main/java/com/example/prusp/wloszczyzna_simple_app/MainActivity.java
+++ app/src/main/java/com/example/prusp/wloszczyzna_simple_app/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.prusp.wloszczyzna_simple_app;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTheCollapsingToolbarTitle();
+    }
+
+    private void setTheCollapsingToolbarTitle() {
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing);
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.CustomTitleStyle);
     }
 
     public void contactOnClick(View view) {
@@ -19,4 +26,6 @@ public class MainActivity extends AppCompatActivity {
         contactIntent.setData(Uri.parse("tel:"+getString(R.string.telephone_number)));
         startActivity(contactIntent);
     }
+
+
 }
